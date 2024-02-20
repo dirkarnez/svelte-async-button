7dfd557380676e795eb9f37531180e58b9919eed
Dirk Arnez <smalldirkalex@gmail.com>
2024-02-20 22:02:39 +0800

    - add Nested.svelte

---
+++ Nested.svelte
@@ -0,0 +1,44 @@
+
+<script>
+		import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+	
+	let classes = "button is-link";
+	let text = "Do"
+	let lastState = NaN;
+	export let promiseFunc; 
+
+	function handleMessage(event) {
+		dispatch(event);
+	}
+	
+	function _doStuff() {
+		text = "Doing";
+		lastState = NaN;
+		promiseFunc()
+		.then(() => {
+			lastState = 1;
+		})
+		.catch((e) => {
+			lastState = 0;
+		});
+		// .finally(() => {
+		// 	lastState = NaN;
+		// 	text = "Do";
+		// })
+	}
+</script>
+
+
+<button class={classes} on:click={_doStuff}>
+	{#if !isNaN(lastState)}
+		{#if  lastState === 0}
+			 <i class="fa-solid fa-check"></i>
+		{:else if lastState === 1}
+			<i class="fa-solid fa-x"></i>
+		{/if}
+		&nbsp;
+	{/if}
+	{text}
+</button>
