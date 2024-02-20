73cdabb7dc2dbf1f5f81b1a15b92b4c39d0d9129
Dirk Arnez <smalldirkalex@gmail.com>
2024-02-20 22:12:34 +0800

    - update Nested.svelte

--- Nested.svelte
+++ Nested.svelte
@@ -19,23 +19,28 @@
 		promiseFunc()
 		.then(() => {
 			lastState = 1;
+			dispatch("success");
 		})
 		.catch((e) => {
 			lastState = 0;
-		});
-		// .finally(() => {
-		// 	lastState = NaN;
-		// 	text = "Do";
-		// })
+			dispatch("failure");
+		})
+		.then(() => new Promise((res) => {
+			setTimeout(res, 2000);
+		}))
+		.finally(() => {
+			lastState = NaN;
+			text = "Do";
+		})
 	}
 </script>
 
 
 <button class={classes} on:click={_doStuff}>
 	{#if !isNaN(lastState)}
-		{#if  lastState === 0}
+		{#if  lastState === 1}
 			 <i class="fa-solid fa-check"></i>
-		{:else if lastState === 1}
+		{:else if lastState === 0}
 			<i class="fa-solid fa-x"></i>
 		{/if}
 		&nbsp;
